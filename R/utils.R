e5bf40c0ed2bb83289c885362bbad19303c8ce65
bjorn <bbartholdy@github.com>
2021-03-31 11:56:40 +0200

    add function to generate NAs asymmetrically columnwise

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,33 @@ missing_vector <- function(x, na_prob){
   return(x)
 }
 
+#' @inheritParams missing
+missing_symm <- function(x, na_prob = 0.1, cols = seq_along(x)){
+  if(na_prob > 1){
+    na_prob <- na_prob / 100
+    warning(paste("'missing_prob' must be a probability between 0 and 1 \n
+              Input was automatically replaced by", na_prob))
+  }
+  if(is.null(dim(x))){
+    x <- missing_vector(x, na_prob = na_prob)
+  } else {
+    x[c(cols)] <- lapply(x[c(cols)], missing_vector, na_prob = na_prob)
+  }
+  return(x)
+}
+
+#' @inheritParams missing
+missing_asymm <- function(x, na_prob = 0.1, cols = seq_along(x)){
+  x_na <- x[cols]
+  n_vect <- nrow(x_na) * ncol(x_na)
+  na_vector <- c(rep(1, n_vect))
+  na_vector[sample(1:length(na_vector), size = 0.1 * n_vect)] <- NA
+  na_matrix <- is.na(matrix(na_vector, nrow = nrow(x_na), ncol = ncol(x_na)))
+  x_na[na_matrix] <- NA
+  x[cols] <- x_na
+  return(x)
+}
+
 # From 'wakefield' package
 #' Inserts NAs throughout a data frame at random
 #'
@@ -14,17 +41,19 @@ missing_vector <- function(x, na_prob){
 #' Default is 0.1, i.e. each variable will have 10% NAs.
 #' @param cols numeric. In which columns to insert NAs. Default is all columns.
 #' To leave out the first column, use cols = -1.
+#' @param symm logical. whether the missing values should be inserted symmetrically,
+#' i.e., whether each column should contain the same number of NAs.
 #' @seealso \code{\link[wakefield]{r_na}}
-missing <- function(x, na_prob = 0.1, cols = seq_along(x)){
+missing <- function(x, na_prob = 0.1, cols = seq_along(x), symm = T){
   if(na_prob > 1){
     na_prob <- na_prob / 100
     warning(paste("'missing_prob' must be a probability between 0 and 1 \n
               Input was automatically replaced by", na_prob))
   }
-  if(is.null(dim(x))){
-    x <- missing_vector(x, na_prob = na_prob)
-  } else {
-  x[c(cols)] <- lapply(x[c(cols)], missing_vector, na_prob = na_prob)
+  if(symm == TRUE){
+    x <- missing_symm(x, na_prob, cols)
+  } else if(symm == FALSE) {
+    x <- missing_asymm(x, na_prob, cols)
   }
   return(x)
 }
@@ -33,7 +62,7 @@ missing <- function(x, na_prob = 0.1, cols = seq_along(x)){
 #'
 #' @param n number of individuals to generate.
 #' @param race which races to include in the generated sample. Default is all.
-#' @param missing_prob Numeric. Proportion of values that should be missing at random.
+#' @param na_prob Numeric. Proportion of values that should be missing at random.
 #' Must be a number between 0 and 1.
 #' @param ... can be used to pass 'stats' through to race-specific functions. see example.
 #' @importFrom dplyr as_tibble
