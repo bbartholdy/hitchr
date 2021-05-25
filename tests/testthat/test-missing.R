0f1521a7413624e7278a71bad58f48a96383a23f
bbartholdy <bbartholdy@github.com>
2021-05-25 15:53:11 +0200

    add testing

--- tests/testthat/test-missing.R
+++ tests/testthat/test-missing.R
@@ -3,7 +3,8 @@ na_prob <- 0.1
 hum_samp_na <- missing(hum_samp, na_prob = na_prob)
 hum_vect_na <- hum_samp_na[,2]
 hum_asymm_na <- is.na(missing(hum_samp, na_prob = na_prob, symm = F))
-is_miss <- towel(hum_samp_na)
+is_miss_cols <- towel(hum_samp_na)
+is_miss_rows <- towel(hum_samp_na, rowwise = T, colwise = F)
 
 test_that("Test missing value creator function", {
   expect_equal(dim(hum_samp), dim(hum_samp_na))
@@ -16,5 +17,6 @@ hum_asymm_select <- is.na(missing(hum_samp, na_prob = na_prob, cols = 2:5, symm 
 
 test_that("Test missing values in select columns", {
   expect_equal(mean(colMeans(hum_asymm_select[,2:5])), na_prob)
-  expect_length(is_miss, ncol(hum_samp_na))
+  expect_length(is_miss_cols, ncol(hum_samp_na))
+  expect_length(is_miss_rows, nrow(hum_samp_na))
 })
